cedd51cd8e849da8cb4ce9ce767fd756016cf2d4
Hamza Ahmad <hamza.ahmad@xprolabs.com>
2022-12-01 11:51:42 +0500

    Solved Indentation Issues

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 class ClientsController < ApplicationController
   def index
-    @client = Client.includes(:sales)
+    @client = Client.all(:sales)
   end
+
   def show
     @client = Client.find(params[:id])
   end
+
   def new
     @client = Client.new
   end
+
   def create
     @client = Client.new(client_strong_params)
     if @client.save
@@ -16,8 +19,11 @@ class ClientsController < ApplicationController
       redirect_to clients_path
     end
   end
+
   private
+
   def client_strong_params
     params.require(:client).permit(:name, :contact_no, :age, :email, :address)
   end
+
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,18 +1,22 @@
 # frozen_string_literal: true
 class Client < ApplicationRecord
   has_many :sales
-  # validates :name, presence: true
-  # validates_length_of :name, maximum: 10
-  # after_save :after_save_status
-  # before_save :before_save_status
-  # after_commit :after_commit_status
-  # def after_save_status
-  #   puts "Client Details Are Saved Using Callbacks"
-  # end
-  # def before_save_status
-  #   puts "Client Details Are Saved Using Before Save"
-  # end
-  # def after_commit_status
-  #   puts "Client Details Saved Using After Commit"
-  # end
+  validates :name, presence: true
+  validates_length_of :name, maximum: 10
+  after_save :after_save_status
+  before_save :before_save_status
+  after_commit :after_commit_status
+
+  def after_save_status
+    puts "Client Details Are Saved Using Callbacks"
+  end
+
+  def before_save_status
+    puts "Client Details Are Saved Using Before Save"
+  end
+
+  def after_commit_status
+    puts "Client Details Saved Using After Commit"
+  end
+
 end
